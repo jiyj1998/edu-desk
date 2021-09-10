8060c122b388bfee63824c0e8012a07d950f8755
jiyj <jiyangjin@yeah.net>
2021-09-10 23:07:29 +0800

    增加assets

--- src/main.js
+++ src/main.js
@@ -3,4 +3,11 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+// import css
+import "@/assets/css/bootstrap.min.css"
+//import "@/assets/css/font-awesome.min.css"
+import "@/assets/css/nprogress.css"
+import "@/assets/themes/flat/green.css"
+import "@/assets/css/custom.min.css"
+
 createApp(App).use(store).use(router).mount('#app')
---
+++ src/views/Login.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="login">
+    <a class="hiddenanchor" id="signup"></a>
+    <a class="hiddenanchor" id="signin"></a>
+    <div class="login_wrapper">
+      <div class="animate form login_form">
+        <section class="login_content">
+          <form>
+            <h1>登录</h1>
+            <div>
+              <input type="text" class="form-control" placeholder="用户名" required="" />
+            </div>
+            <div>
+              <input type="password" class="form-control" placeholder="密码" required="" />
+            </div>
+            <div>
+              <a class="btn btn-default submit" href="index.html">登录</a>
+              <a class="reset_pass" href="#">忘记密码?</a>
+            </div>
+
+            <div class="clearfix"></div>
+
+            <div class="separator">
+              <div class="clearfix"></div>
+              <br />
+              <div>
+                <h1>developing!</h1>
+                <p>©2021 All Rights Reserved. jiyangjin@yeah.net</p>
+              </div>
+            </div>
+          </form>
+        </section>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+
+export default {
+  name: 'login',
+}
+</script>
